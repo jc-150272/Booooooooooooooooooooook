de60663b59ad311b69370a675d8a9d48e8bdd8d9
凌 松本 <150272@jc-21.jp>
2018-01-16 16:29:14 +0900

    aa

--- book3/book3/BookPage.xaml.cs
+++ book3/book3/BookPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
