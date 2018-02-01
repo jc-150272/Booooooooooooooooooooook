efaf9595ccfd58d6bf7e3e4e30cded4dd1d522d7
凌 松本 <150272@jc-21.jp>
2018-02-01 14:36:35 +0900

    twwwwwww

--- book3/book3/BookPage.xaml.cs
+++ book3/book3/BookPage.xaml.cs
@@ -13,7 +13,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace book3
-{
+{ 
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BookPage : ContentPage
     {        
