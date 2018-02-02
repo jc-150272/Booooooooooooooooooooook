da15ff223fa9819833b9538832356a7ac759ab27
凌 松本 <150272@jc-21.jp>
2018-02-02 18:30:22 +0900

    addsipasfd

--- book3/book3/BookPage.xaml.cs
+++ book3/book3/BookPage.xaml.cs
@@ -37,7 +37,9 @@ namespace book3
                 var isbnList = new List<String>();
                 var RedList = new List<int>();
                 var BlueList = new List<int>();
-                //*をリストにぶち込んで個数分addするのでもいいのでは
+                var RedList2 = new List<string>();
+                var BlueList2 = new List<string>();
+
                 foreach (var user in query)
                 {
                     titleList.Add(user.Title);
@@ -45,30 +47,45 @@ namespace book3
                     RedList.Add(user.RedStar);
                     BlueList.Add(user.BlueBook);
                 }
+
+                for (var h = 0; h < query.Count; h++)
+                {
+                    if (RedList[h] == 1)
+                    {
+                        RedList2.Add("red_star_72.png");
+                    }
+                    else
+                    {
+                        RedList2.Add("");
+                    }
+
+                    if (BlueList[h] == 1)
+                    {
+                        BlueList2.Add("blue_book_72.png");
+                    }
+                    else
+                    {
+                        BlueList2.Add("");
+                    }
+                }
+
                 for (var j = 0; j < query.Count; j++)
                 {
                     items.Add(new Book { 
-                        Name = titleList[j],ISBN = isbnList[j], RedStar = RedList[j], BlueBook = BlueList[j],});
+                        Name = titleList[j],
+                        ISBN = isbnList[j],
+                        RedStar = RedList[j],
+                        BlueBook = BlueList[j],
+                        RedStar2 = RedList2[j],
+                        BlueBook2 = BlueList2[j]
+                    });
 
                 }
             }
+
             else
             {
                 items.Add(new Book { Name = "空やで" });
-            }
-
-
-            for (var i = 0; i < items.Count; i++)
-            {
-                if (items[i].BlueBook == 1)
-                {
-                    items[i].RedStar2 = "red_star_72.png";
-                }
-                if (items[i].BlueBook == 1)
-                {
-                    items[i].BlueBook2 = "blue_book_72.png";
-                }
-
             }
 
             BookListView.ItemsSource = items;
